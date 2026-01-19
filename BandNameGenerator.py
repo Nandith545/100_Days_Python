708598ca1ec6a6c6982438dfe3052243c90452c8
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-01-18 23:35:53 -0800

    Create BandNameGenerator.py

---
+++ BandNameGenerator.py
@@ -0,0 +1,9 @@
+# This program generates a Band name by taking in the City and Fav pet name from the user inputs
+
+userName = input("Enter your name:\n")
+print("Hello " + userName + "!" + " Welcome to the Band Name Generator")
+
+cityName = input("Enter the name of the city you grew up in:\n")
+petName = input("Enter the name of your pet\n")
+
+print("Your band name could be " + cityName + " " + petName)
