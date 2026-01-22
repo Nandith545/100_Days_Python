7459f7434f93bf23cc91a87483e60c3fd1a6b5ec
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-01-21 23:07:51 -0800

    Update PizzaPriceCalculator.py

--- PizzaPriceCalculator.py
+++ PizzaPriceCalculator.py
@@ -29,3 +29,31 @@ elif size == "L":
 else:
     final_bill = 0
     print(f"Invalid selection ${final_bill}")
+
+# Improvised Version
+
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M or L: ")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+final_bill = 0
+
+if size == "S":
+    final_bill += 15
+elif size == "M":
+    final_bill += 20
+elif size == "L":
+    final_bill += 25
+else:
+ print("You have chosen an invalid size.")
+
+if pepperoni == "Y":
+     if size == "S":
+        final_bill += 2
+     else:
+         final_bill += 3
+
+if extra_cheese == "Y":
+    final_bill += 1
+
+print(f"Your final bill is: ${final_bill}.")
